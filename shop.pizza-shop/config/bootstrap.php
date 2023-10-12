e2df3a43dc71d254b44e6907e4d2c019d9fc4fcb
Jules <bresson23u@etu.univ-lorraine.fr>
2023-10-12 16:29:35 +0200

    Exo 3 TD3, Pas sûr du tout de ce que j'ai fais mais les tests passent tjrs

--- shop.pizza-shop/config/bootstrap.php
+++ shop.pizza-shop/config/bootstrap.php
@@ -1,17 +1,26 @@
 <?php
 
-
+use Illuminate\Container\Container;
 use Illuminate\Database\Capsule\Manager as Eloquent;
 use Slim\Factory\AppFactory as Factory;
+use pizzashop\shop\app\actions\get\AccederCommande;
 
 $app = Factory::create();
-$app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, false, false);
+
+$container = new Container();
+Factory::setContainer($container);
 
 $eloquent = new Eloquent();
 $eloquent->addConnection(parse_ini_file(__DIR__ . '/commande.db.ini'), 'commande');
 $eloquent->addConnection(parse_ini_file(__DIR__ . '/catalog.db.ini'), 'catalog');
 $eloquent->setAsGlobal();
 $eloquent->bootEloquent();
+
+$container->bind('commande.service', function () {
+    return new AccederCommande();
+});
+
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, false, false);
 
 return $app;
--- shop.pizza-shop/src/app/actions/get/AccederCommande.php
+++ shop.pizza-shop/src/app/actions/get/AccederCommande.php
@@ -4,18 +4,16 @@ namespace pizzashop\shop\app\actions\get;
 
 use Exception;
 use pizzashop\shop\app\actions\AbstractAction;
-use pizzashop\shop\domain\service\CatalogueService;
-use pizzashop\shop\domain\service\CommandeService;
 use pizzashop\shop\Exception\ServiceCommandeNotFoundException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AccederCommande extends AbstractAction
 {
-
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-        $commandeService = new CommandeService();
+        $commandeService = $this->container->get('commande.service');
+
         try {
             $commande = $commandeService->accederCommande($args['id_commande']);
             $data = [
@@ -37,9 +35,11 @@ class AccederCommande extends AbstractAction
             $data = $e->getMessage();
             $status = 500;
         }
+
         $data = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         $data = str_replace('\/', '/', $data);
         $response->getBody()->write($data);
+
         return $response->withHeader('Content-Type', 'application/json')
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withStatus($status);
