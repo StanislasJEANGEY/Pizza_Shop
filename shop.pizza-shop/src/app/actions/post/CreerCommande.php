4b5a14edd11ac0bcf14869eb1da161a2a08d43dd
Stanislas <jeangey1u@etu.univ-lorraine.fr>
2023-10-20 11:40:28 +0200

    le TD3 est fini

--- shop.pizza-shop/src/app/actions/post/CreerCommande.php
+++ shop.pizza-shop/src/app/actions/post/CreerCommande.php
@@ -11,6 +11,8 @@ use pizzashop\shop\domain\service\iCommandeService;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Respect\Validation\Exceptions\ValidationException;
+use Respect\Validation\Validator as v;
 
 class CreerCommande extends AbstractAction
 {
@@ -26,27 +28,40 @@ class CreerCommande extends AbstractAction
     public function __invoke(Request $request, Response $response, array $args): Response
     {
         $body = $request->getBody();
-        //TODO : gérer les exceptions
+        try{
+            $body = json_decode($body, true);
+            $array_items = [];
+            foreach ($body['items'] as $item) {
+                $array_items[] = new ItemDTO("",
+                    $item['numero'],
+                    $item['quantite'],
+                    (float)null,
+                    "",
+                    '',
+                    $item['taille']);
+            }
 
-        $body = json_decode($body, true);
-        $array_items = [];
-        foreach ($body['items'] as $item) {
-            $array_items[] = new ItemDTO("",
-                $item['numero'],
-                $item['quantite'],
-                (float)null,
-                "",
-                '',
-                $item['taille']);
-        }
+            $commandeDTO = new CommandeDTO("", "", $body['type_livraison'], (int)null, (int)null, (float)null, $body['mail_client'], $array_items);
 
-        $commandeDTO = new CommandeDTO("","", $body['type_livraison'], (int)null, (int)null, (float)null, $body['mail_client'], $array_items);
-
-        try {
             $id = $this->commandeService->creerCommande($commandeDTO);
             $data = AccederCommande::accederCommandeToJSON($id, $this->commandeService, $this->container);
             $status = 201;
+            $response->withHeader('Location', $this->container->get('settings') . '/commande/' . $id);
 
+        } catch (ValidationException $e) {
+            $data = [
+                'message' => $e->getCode().' Internal Server Error',
+                'exception' => [
+                    [
+                        'type' => get_class($e),
+                        'code' => $e->getCode(),
+                        'message' => $e->getFullMessage(),
+                        'file' => $e->getFile(),
+                        'line' => $e->getLine()
+                    ]
+                ]
+            ];
+            $status = 400;
         } catch (Exception $e) {
             $data = $this->exception($e);
             $status = $e->getCode();
