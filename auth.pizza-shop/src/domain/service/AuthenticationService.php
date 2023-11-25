eb642ebe4c430aa48741a305a3a1862b537b2c7a
Stanislas <jeangey1u@etu.univ-lorraine.fr>
2023-11-25 12:21:01 +0100

    refresh fonction et l'update aussi

--- auth.pizza-shop/src/domain/service/AuthenticationService.php
+++ auth.pizza-shop/src/domain/service/AuthenticationService.php
@@ -4,6 +4,7 @@ namespace pizzashop\auth\api\domain\service;
 
 use Exception;
 use MongoDB\Driver\Exception\AuthenticationException;
+use pizzashop\auth\api\domain\dto\UserDTO;
 use pizzashop\auth\api\domain\entities\User;
 use pizzashop\auth\api\domain\provider\AuthenticationProvider;
 use pizzashop\auth\api\domain\service\utils\JWTManager;
@@ -21,26 +22,33 @@ class AuthenticationService
         $this->authenticationProvider = $authenticationProvider;
     }
 
+    private function generateToken(string $email) : array
+    {
+        $userDTO = $this->authenticationProvider->getUserProfile($email);
+        if ($userDTO) {
+            $tokenData = [
+                "username" => $userDTO->username,
+                "email" => $userDTO->email
+            ];
+            $accessToken = $this->jwtManager->createToken($email, $tokenData);
+            $refreshToken = Uuid::uuid4();
+            $user = User::where('email', $email)->first();
+            $user->refresh_token = $refreshToken;
+            $user->refresh_token_expiration_date = date("Y-m-d H:i:s", strtotime("+1 month"));
+            $bool = $user->update();
+            if ($bool)
+                return ['access_token' => $accessToken, 'refresh_token' => $refreshToken];
+            else throw new Exception("Update failed", 500);
+        }
+        throw new Exception("User not found", 404);
+    }
     /**
      * @throws Exception
      */
     public function signin(string $email, string $password): array
     {
         if($this->authenticationProvider->authenticateWithCredentials($email, $password)){
-            $userDTO = $this->authenticationProvider->getUserProfile($email);
-            if ($userDTO) {
-                $tokenData = [
-                    "username" => $userDTO->username,
-                    "email" => $userDTO->email
-                ];
-                $accessToken = $this->jwtManager->createToken($email, $tokenData);
-                $refreshToken = Uuid::uuid4();
-                $userDTO->refresh_token = $refreshToken;
-                $userDTO->refresh_token_expiration_date = date("Y-m-d H:i:s", time() + 3600);
-                if ($userDTO->toUser()->update())
-                    return ['access_token' => $accessToken, 'refresh_token' => $refreshToken];
-                else throw new Exception("Update failed", 500);
-            }
+            return $this->generateToken($email);
         }
         throw new AuthenticationException("Authentication failed", 401);
     }
@@ -61,13 +69,14 @@ class AuthenticationService
     public function refresh(string $refreshToken): array
     {
             $user = $this->authenticationProvider->authenticateWithRefreshToken($refreshToken);
-            if (isNull($user)) throw new Exception("Invalid refresh token", 401);
-            else {
-                if ($user->refresh_token_expiration_date < date("Y-m-d H:i:s")) throw new Exception("Refresh token expired", 401);
+            if (isset($user)) {
+                if (date("Y-m-d H:i:s") < $user->refresh_token_expiration_date)
+                    return $this->generateToken($user->email);
                 else {
-                    return $this->signin($user->email, $user->password);
+                    throw new Exception("Refresh token expired", 401);
                 }
-            }
+            } else throw new Exception("Invalid refresh token", 401);
+
     }
 
     public function signup(string $username, string $email, string $password) //: array
