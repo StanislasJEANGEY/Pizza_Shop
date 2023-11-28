1f11b3378b95e6a794c3fd160f1357b26d722bb5
BJulOs <julesbresson88600@gmail.com>
2023-11-28 17:06:31 +0100

    J'ai fais des trucs (rien de compliqué à retrouver si problème) mais je ne sais pas comment tester

--- shop.pizza-shop/src/app/actions/post/AuthentificationRefresh.php
+++ shop.pizza-shop/src/app/actions/post/AuthentificationRefresh.php
@@ -3,14 +3,33 @@
 namespace pizzashop\shop\app\actions\post;
 
 use pizzashop\shop\app\actions\AbstractAction;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AuthentificationRefresh extends AbstractAction
 {
 
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-        // TODO: Implement __invoke() method.
+        // récupération du token en provenance du client
+        $token = $request->getHeader('Authorization');
+        // renvoie vers l'api authentification avec le token avec guzzle
+        if (isset($token[0])) {
+            // renvoie la réponse de l'api authentification
+            return $this->container->get('guzzle')->request('POST', $this->container->get('link_auth') . 'refresh', [
+                'headers' => [
+                    'Authorization' => $token[0]
+                ]
+            ]);
+        } else {
+            // renvoie une erreur si le token n'est pas présent
+            return $response->withStatus(401);
+        }
     }
 }
--- shop.pizza-shop/src/app/actions/post/AuthentificationSingin.php
+++ shop.pizza-shop/src/app/actions/post/AuthentificationSingin.php
@@ -3,24 +3,34 @@
 namespace pizzashop\shop\app\actions\post;
 
 use pizzashop\shop\app\actions\AbstractAction;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AuthentificationSingin extends AbstractAction
 {
 
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function __invoke(Request $request, Response $response, array $args): Response
     {
         //récupération les credentials en basic auth
         $credentials = $request->getHeader('Authorization');
         //renvoie vers l'api authentification avec les credentials avec guzzle
-        $response = $this->container->get('guzzle')->request('POST', $this->container->get('link_auth') . 'signin', [
-            'headers' => [
-                'Authorization' => $credentials
-            ]
-        ]);
-        //renvoie la réponse de l'api authentification
-        return $response;
+        if (isset($credentials[0])) {
+            //renvoie la réponse de l'api authentification
+            return $this->container->get('guzzle')->request('POST', $this->container->get('link_auth') . 'singin', [
+                'headers' => [
+                    'Authorization' => $credentials[0]
+                ]
+            ]);
+        } else {
+            //renvoie une erreur si les credentials ne sont pas présents
+            return $response->withStatus(401);
+        }
 
     }
 }
