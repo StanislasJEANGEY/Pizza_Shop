03d5d390a5e4a621980c91cbf9b255d80dfa6008
Stanislas <jeangey1u@etu.univ-lorraine.fr>
2024-01-08 11:22:04 +0100

    conf pour dev

---
+++ shop.pizza-shop/tests/commande/ServiceCatalogueTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace commande;
+
+use Illuminate\Database\Capsule\Manager as DB;
+use PHPUnit\Framework\TestCase;
+use pizzashop\shop\domain\service\CatalogueService as CatalogueService;
+use pizzashop\shop\domain\service\CommandeService as CommandeService;
+
+class ServiceCatalogueTest extends TestCase
+{
+    private static $serviceProduits;
+    private static $serviceCommande;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        $dbcom = __DIR__ . '/../../config/commande.db.ini';
+        $dbcat = __DIR__ . '/../../config/catalog.db.ini';
+        $db = new DB();
+        $db->addConnection(parse_ini_file($dbcom), 'commande');
+        $db->addConnection(parse_ini_file($dbcat), 'catalog');
+        $db->setAsGlobal();
+        $db->bootEloquent();
+
+        self::$serviceProduits = new CatalogueService();
+        self::$serviceCommande = new CommandeService(self::$serviceProduits);
+
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+
+    }
+
+}
