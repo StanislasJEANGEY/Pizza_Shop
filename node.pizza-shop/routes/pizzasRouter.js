66decd7883eee22fda2b910ae032bb406072f9d1
Damien <damien.mel.2404@mail.com>
2024-02-02 16:57:29 +0100

    debut td9

--- node.pizza-shop/routes/pizzasRouter.js
+++ node.pizza-shop/routes/pizzasRouter.js
@@ -1,4 +1,20 @@
 import express from "express";
+import Knex from "knex";
+import ServiceCommande from "../services/ServiceCommande.js";
+import ListerCommandes from "../actions/ListerCommandes.js";
+
+const knex = Knex({
+    client: 'mysql',
+    connection: {
+        host: 'node.pizza-shop.db',
+        user: 'pizza_shop',
+        password: 'pizza_shop',
+        database: 'pizza_shop'
+    }
+});
+
+const serviceCommande = new ServiceCommande(knex);
+const listerCommandes = new ListerCommandes(serviceCommande);
 
 const router = express.Router();
 
@@ -8,5 +24,7 @@ router
     res.json([]);
   }) //call getPizzasAction method and transmit req, res, next argument
   .all((req, res, next) => next(405)); //method not allowed
+
+router.get('/commandes', (req, res) => listerCommandes.executer(req, res));
 
 export default router;
